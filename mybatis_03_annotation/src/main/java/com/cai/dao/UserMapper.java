a1e518e4f8976ebfdb777e2ea28ca2c3925dbaa4
pengcai6 <3538616223@qq.com>
2024-07-13 16:54:17 +0800

    mybatis_03_annotation注解开发新增根据给定的名字模糊查询拥用户数据

--- mybatis_03_annotation/src/main/java/com/cai/dao/UserMapper.java
+++ mybatis_03_annotation/src/main/java/com/cai/dao/UserMapper.java
@@ -1,7 +1,9 @@
 package com.cai.dao;
 
 import com.cai.pojo.User;
-import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
 
 public interface UserMapper {
 
@@ -12,12 +14,35 @@ public interface UserMapper {
       */
      @Insert("insert into tb_user (user_name, password, name, age, sex) " +
              "values (#{userName},#{password},#{name},#{age},#{sex})")
-     int insertUser(User user);
+     int addUser(User user);
+     @Insert("insert into tb_user (user_name, password, name, age, sex) " +
+             "values (#{userName},#{password},#{name},#{age},#{sex})")
+     @Options(useGeneratedKeys = true ,keyColumn = "id",keyProperty = "id")
+     //开启主键回填：useGeneratedKeys = true 表中字段名 keyColumn = "id", 实体类中的属性名： keyProperty = "id"
+     int addUserGetPk(User user);
+
 
      /**
-      * 根据用户id查询用户信息以及下单信息
-      * @param id 用户id
-      * @return 用户对象
+      * 查询所有用户
+      * @return 用户对象列表
       */
-     User findUserById(Integer id);
+     @Select("select id as user_id, user_name, password, name, age, sex from tb_user")
+     @Results(
+             id = "useMap",
+             value = {
+                     @Result(column = "user_id",property = "id" ,id=true),
+                     @Result(column = "user_name",property = "userName")
+             }
+     )
+     List<User> FindAllUser();
+
+     /**
+      * 根据给定的名字模糊查询拥用户数据
+      * @param name 名字
+      * @return 存储用户对象的列表
+      */
+     @Select("SELECT  id , user_name as userName, password, name, age,sex " +
+             "FROM tb_user WHERE sex = 1 and name like  concat('%',#{name},'%') ")
+     List<User> FindUserByLike(String name);
+
 }
--- mybatis_03_annotation/src/test/java/com/cai/UserMapperTest.java
+++ mybatis_03_annotation/src/test/java/com/cai/UserMapperTest.java
@@ -4,32 +4,66 @@ import com.cai.pojo.User;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 
+import java.util.List;
+
 import static com.cai.util.mybatisUtil.getSession;
 
 public class UserMapperTest {
     @Test
-    public void findUserByIdTest(){
+    public void findAllUserTest(){
         SqlSession sqlSession = getSession();
         UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
-        User user = userMapper.findUserById(1);
+        List<User> userList=userMapper.FindAllUser();
+        for (User user : userList) {
+            System.out.println("用户信息"+user);
+        }
         sqlSession.close();
-        System.out.println("用户信息"+user);
     }
     @Test
-    public void insertUserTest(){
+    public void findUserByLike(){
+        SqlSession sqlSession = getSession();
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+        List<User> userList=userMapper.FindUserByLike("张");
+        for (User user : userList) {
+            System.out.println("用户信息"+user);
+        }
+        sqlSession.close();
+    }
+
+    @Test
+    public void addUserTest(){
         SqlSession sqlSession = getSession();//开启自动事务
         UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
         User user=new User();
-        user.setUserName("123");
+        user.setUserName("1243");
         user.setAge(12);
         user.setName("123");
         user.setPassword("123");
         user.setSex(0);
-        int rowCount = userMapper.insertUser(user);
+        int rowCount = userMapper.addUser(user);
         if(rowCount>0)
         {
             System.out.println("插入成功");
         }
+        sqlSession.close();
+    }
+    @Test
+    public void addUserGePkTest(){
+        SqlSession sqlSession = getSession();//开启自动事务
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+        User user=new User();
+        user.setUserName("李博");
+        user.setAge(1);
+        user.setName("123123");
+        user.setPassword("123456");
+        user.setSex(1);
+        System.out.println(user);
+        int rowCount = userMapper.addUserGetPk(user);
+        if(rowCount>0)
+        {
+            System.out.println("插入成功");
+        }
+        System.out.println("主键回填后"+user);
         sqlSession.close();
     }
 
