6aed9092fdfa8a39c309d215e51ed528072c46c3
pengcai6 <3538616223@qq.com>
2024-07-25 20:34:16 +0800

    自己做的作业，servlet能通过request对象接收页面发送过来的数据，并解决post请求乱码

---
+++ work01-2024-7-25/src/main/java/com/cai/pojo/dish.java
@@ -0,0 +1,14 @@
+package com.cai.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class dish {
+    double price;
+    String name;
+    String description;
+}
